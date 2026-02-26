bebc6ccfcce2bdf7b355c3f20375766ea0f6adeb
Pedro Antoninho <pdn.antoninho@gmail.com>
2026-02-26 17:14:04 +0000

    chore: coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: true,
-        root: '.'
+        root: '.',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov', 'clover', 'json'],
+            reportsDirectory: './coverage'
+        }
     }
 });
